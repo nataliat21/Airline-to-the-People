58a03c3d64fcef1151305d02a4c080aaaddfd4a5
nataliat21 <natalia.torozco21@gmail.com>
2023-10-19 14:57:23 -0500

    Functionality is added to the menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,9 +8,15 @@ import { Component } from '@angular/core';
 export class NavbarComponent {
 
   showModal = false; /*Property to control the visibility of the modal window*/
+  showMenu = false; /* Variable to control the visibility of navbar-mobile-menu*/
 
     /* Method to toggle modal window visibility*/
     toggleModal() {
       this.showModal = !this.showModal;
+    }
+
+    /* Feature to toggle menu visibility*/
+    toggleMenu() {
+      this.showMenu = !this.showMenu;
     }
 }
